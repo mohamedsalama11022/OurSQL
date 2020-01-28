0a529cc3f08f9c52577dcc39db07c9c99dd31573
mohamedsalama11022 <58034439+mohamedsalama11022@users.noreply.github.com>
2020-01-29 00:43:34 +0200

    Update oursql.sh

--- oursql.sh
+++ oursql.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 select choice in createDB showDataBases
-do
-case $choice in
-createDB) echo Enter The DataBase Name:
-read dbName
-echo "the dbName is $dbName"
-break
-;;
-showDataBases) select operation in useDataBase
-do
-case $operation in
-useDataBase) echo Enter The DataBase name you want to use
-select tblOperation in createTable DeleteTable InsertIntoTable
-do
-case $tblOperation in 
-createTable) echo "test creating the table"
-;;
-DeleteTable) echo "test Deleting the table"
-;;
-InsertIntoTable) echo "test insering into the table"
-;;
-esac
-done
-esac
-done
-esac
-done
+	do
+		case $choice in
+			createDB) echo Enter The DataBase Name:
+	read dbName
+	echo "the dbName is $dbName"
+	break
+	;;
+			showDataBases) select operation in useDataBase
+			do
+				case $operation in
+					useDataBase) echo Enter The DataBase name you want to use
+	select tblOperation in createTable DeleteTable InsertIntoTable
+					do
+						case $tblOperation in 
+						createTable) echo "test creating the table"
+						;;
+						DeleteTable) echo "test Deleting the table"
+						;;
+						InsertIntoTable) echo "test insering into the table"
+						;;
+						esac
+					done
+				esac
+			done
+		esac
+	done
 
