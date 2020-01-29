86cc18e3be2bc701b63a210c9cc1c7a867798de7
Muhammad ALsaied <muhammadalsaied96@gmail.com>
2020-01-29 20:45:47 +0200

    basic features

---
+++ oursql.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+function createDB {
+	echo Enter The DataBase Name:
+	read dbName
+	mkdir -p ~/oursql/${dbName}
+	echo "$dbName was created successfuly"
+}
+
+function dbOperations {
+	select tblOperation in "create table" "delete table" "insert into table" "back"
+		do
+			case $tblOperation in 
+				"create table") echo "test creating the table"
+				;;
+				"delete table") echo "test Deleting the table"
+				;;
+				"insert tnto table") echo "test insering into the table"
+				;;
+				"back") showDatabases
+				;;
+			esac
+		done
+}
+function showDatabases {
+	select operation in `ls ~/oursql`
+		do
+			echo "The database you select is ${operation}"
+			dbOperations
+		done
+}
+
+
+select choice in "create database" "show dataBases"
+do
+	case $choice in
+		"create database")
+			createDB
+			;;
+		"show dataBases")
+			showDatabases
+		;;
+	esac
+done
